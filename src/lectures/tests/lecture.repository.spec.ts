30e4d3299ca34e7c30b689d658b0e32cc17d558a
Nicolas Beaulieu-Gagnon <63028085+NicolasBeaulieuGagnon@users.noreply.github.com>
2022-01-23 21:57:35 -0500

    created a test for lectureRepository, not sure if right direction

--- src/lectures/tests/lecture.repository.spec.ts
+++ src/lectures/tests/lecture.repository.spec.ts
@@ -1,88 +1,39 @@
 import { Test } from '@nestjs/testing';
-import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/auth/user.entity';
-import { Repository } from 'typeorm';
-import { LectureAttendanceRepository } from '../lecture-attendance.repository';
-import { Lecture } from '../lecture.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LectureRepository } from '../lecture.repository';
-import { mockUser } from './lecture.mocks';
-
-const mockLectureRepoObject: Lecture = {
-  lecture_id: '25740dc0-44dc-4f11-b5d6-1ccfe4a06abb',
-  name: 'class name',
-  user_id: 'aa36d01b-f427-4366-ae03-250d7edd70de',
-  description: 'some description',
-  start_date: '235576',
-  end_date: ' 325346',
-  students: 0,
-};
-
-const mockUserRepoObject: User = {
-  ...mockUser,
-  user_id: 'aa36d01b-f427-4366-ae03-250d7edd70de',
-};
 
 describe('Lecture Repository', () => {
   let lectureRepository: LectureRepository;
-  let entity: Repository<Lecture>;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       imports: [
-        TypeOrmModule.forRoot({
-          type: 'postgres',
-          host: 'localhost',
-          port: 5432,
-          username: 'postgres',
-          password: 'postgres',
-          database: 'class-creator-test-DB',
-          dropSchema: true,
-          entities: [Lecture],
-          synchronize: true,
-          autoLoadEntities: true,
-          keepConnectionAlive: false,
+        TypeOrmModule.forRootAsync({
+          useFactory: async () => {
+            return {
+              type: 'postgres',
+              autoLoadEntities: true,
+              synchronize: true,
+              host: 'localhost',
+              port: 5432,
+              username: 'postgres',
+              password: 'postgres',
+              database: 'class-creator-DB-test',
+            };
+          },
         }),
-        TypeOrmModule.forFeature([
-          Lecture,
-          LectureRepository,
-          LectureAttendanceRepository,
-        ]),
+        TypeOrmModule.forFeature([LectureRepository]),
       ],
     }).compile();
-    lectureRepository = module.get(LectureRepository);
-    entity = module.get(getRepositoryToken(Lecture));
+    lectureRepository = module.get<LectureRepository>(LectureRepository);
   });
 
-  it('should be defined', () => {
-    expect(lectureRepository).toBeDefined();
-  });
+  describe('find', () => {
+    it('should return an empty array if it finds nothing', async () => {
+      const lectures = await lectureRepository.find();
 
-  afterEach(async () => {
-    await entity.manager.connection.close();
-  });
-
-  describe('getLectures', () => {
-    it('should return an empty array if no lectures were found', async () => {
-      const result = await lectureRepository.getLectures(
-        '',
-        false,
-        mockUserRepoObject,
-      );
-
-      expect(result).toEqual([]);
+      console.log(lectures);
+      expect(lectures).toEqual([]);
     });
   });
-
-  describe('createLecture', () => {
-    it('should create an new lecture and return nothing', async () => {
-      const result = await lectureRepository.createLecture(
-        mockLectureRepoObject,
-        mockUserRepoObject,
-      );
-
-      expect(result).toEqual(undefined);
-    });
-  });
-
-  describe('', () => {});
 });
